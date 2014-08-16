672d6855ace1e9d6a8f7710d4ab236cbc5fdb10f
Andrew Kuchev <0coming.soon@gmail.com>
2014-08-16 20:50:55 +0600

    Added example (load submissions for contest by it's id)

---
+++ examples/load_submissions_for_contest/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""
+In this example we are loading submissions for contest by it's id
+"""
+
+import os
+import sys
+
+from api import CodeforcesAPI
+
+
+def main(argv):
+    api = CodeforcesAPI()
+
+    contest_id = int(argv[1])
+
+    submissions = api.contest_status(contest_id)
+
+    print('{:^20}{}'.format('Submission ID', 'Party members'))
+
+    for s in submissions:
+        print('{:^20}{}'.format(s.id, ', '.join(member.handle for member in s.author.members)))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        main(sys.argv)
+    else:
+        print("Invalid number of arguments")
+        print("Usage: python {} [contest id]".format(os.path.basename(sys.argv[0])))

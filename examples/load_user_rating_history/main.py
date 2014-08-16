7f649671f990d35876e0bf428cc336573586c8c3
Andrew Kuchev <0coming.soon@gmail.com>
2014-08-16 21:16:58 +0600

    Added example (load user rating history by it's handle)

---
+++ examples/load_user_rating_history/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+In this example we are loading users rating history by it's handle
+"""
+
+import os
+import sys
+
+from api import CodeforcesAPI
+
+
+def main(argv):
+    api = CodeforcesAPI()
+
+    handle = argv[1]
+
+    rating_changes = api.user_rating(handle)
+
+    print('Rating history for {}:'.format(handle))
+    for rating in rating_changes:
+        print(rating.old_rating, end=' -> ')
+
+    print(rating_changes[-1].new_rating)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        main(sys.argv)
+    else:
+        print("Invalid number of arguments")
+        print("Usage: python {} [contest id]".format(os.path.basename(sys.argv[0])))

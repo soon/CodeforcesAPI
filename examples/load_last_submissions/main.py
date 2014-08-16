c53d57672881f402919058235ce2ad876cd7105a
Andrew Kuchev <0coming.soon@gmail.com>
2014-08-16 21:05:27 +0600

    Added example (load N last submissions)

---
+++ examples/load_last_submissions/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""
+In this example we are loading last N submissions
+"""
+
+import os
+import sys
+
+from api import CodeforcesAPI
+
+
+def main(argv):
+    api = CodeforcesAPI()
+
+    count_submissions = int(argv[1])
+
+    submissions = api.problemset_recent_status(count_submissions)
+
+    print('{:^20}{}'.format('Submission ID', 'Party members'))
+
+    for s in submissions:
+        print('{:^20}{}'.format(s.id, ', '.join(member.handle for member in s.author.members)))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        main(sys.argv)
+    else:
+        print("Invalid number of arguments")
+        print("Usage: python {} [contest id]".format(os.path.basename(sys.argv[0])))

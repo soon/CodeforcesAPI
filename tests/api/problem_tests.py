186698bf74a2c7f057235fa44e2bd30586d9a7e7
Andrew Kuchev <0coming.soon@gmail.com>
2014-08-20 12:11:19 +0600

    Testing: Added basic tests for Problem

---
+++ tests/api/problem_tests.py
@@ -0,0 +1,93 @@
+"""
+This module provides classes for testing Problem object
+"""
+
+import unittest
+from api import Problem
+from api import ProblemType
+
+
+class ProblemTests(unittest.TestCase):
+
+    def setUp(self):
+        self.problem = Problem()
+
+    def test_load_from_dict(self):
+        d = {
+            "contestId": 374,
+            "index": "A",
+            "name": "Inna and Pink Pony",
+            "type": "PROGRAMMING",
+            "points": 500.0,
+            "tags": ["greedy", "implementation"]
+        }
+
+        self.problem.load_from_dict(d)
+
+        self.assertEqual(374, self.problem.contest_id)
+        self.assertEqual('A', self.problem.index)
+        self.assertEqual('Inna and Pink Pony', self.problem.name)
+        self.assertEqual(ProblemType.programming, self.problem.type)
+        self.assertEqual(500.0, self.problem.points)
+        self.assertEqual(["greedy", "implementation"], self.problem.tags)
+
+    def load_only_required_from_dict(self):
+        d = {
+            "contestId": 374,
+            "index": "A",
+            "name": "Inna and Pink Pony",
+            "type": "PROGRAMMING",
+            "tags": ["greedy", "implementation"]
+        }
+
+        self.problem.load_from_dict(d)
+
+        self.assertEqual(374, self.problem.contest_id)
+        self.assertEqual('A', self.problem.index)
+        self.assertEqual('Inna and Pink Pony', self.problem.name)
+        self.assertEqual(ProblemType.programming, self.problem.type)
+        self.assertEqual(["greedy", "implementation"], self.problem.tags)
+
+        self.assertIsNone(self.problem.points)
+
+    def test_load_from_json(self):
+        json = '''{
+            "contestId": 374,
+            "index": "A",
+            "name": "Inna and Pink Pony",
+            "type": "PROGRAMMING",
+            "points": 500.0,
+            "tags": ["greedy","implementation"]
+        }'''
+
+        self.problem.load_from_json(json)
+
+        self.assertEqual(374, self.problem.contest_id)
+        self.assertEqual('A', self.problem.index)
+        self.assertEqual('Inna and Pink Pony', self.problem.name)
+        self.assertEqual(ProblemType.programming, self.problem.type)
+        self.assertEqual(500.0, self.problem.points)
+        self.assertEqual(["greedy", "implementation"], self.problem.tags)
+
+    def load_only_required_from_json(self):
+        json = '''{
+            "contestId": 374,
+            "index": "A",
+            "name": "Inna and Pink Pony",
+            "type": "PROGRAMMING",
+            "tags": ["greedy","implementation"]
+        }'''
+
+        self.problem.load_from_json(json)
+
+        self.assertEqual(374, self.problem.contest_id)
+        self.assertEqual('A', self.problem.index)
+        self.assertEqual('Inna and Pink Pony', self.problem.name)
+        self.assertEqual(ProblemType.programming, self.problem.type)
+        self.assertEqual(["greedy", "implementation"], self.problem.tags)
+
+        self.assertIsNone(self.problem.points)
+
+
+if __name__ == '__main__':
+    unittest.main()
